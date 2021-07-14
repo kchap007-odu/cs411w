d6e36450f4235cd4390f7a30abe47db6e5ca6706
kchappel <kchap007@odu.edu>
2021-07-14 18:46:41 -0400

    Fix failing test cases
    Test cases were failing asserting exactly equal on floats.

--- TestThermostats.py
+++ TestThermostats.py
@@ -38,30 +38,30 @@ class TestThermostats(unittest.TestCase):
 
         t.set_temperature_scale("F")
         t.set_eco_temperature_high(value=70)
-        self.assertEqual(t.eco_temperature_high_f, 70)
+        self.assertAlmostEqual(t.eco_temperature_high_f, 70)
 
         t.set_temperature_scale("C")
         t.set_eco_temperature_high(value=30)
-        self.assertEqual(t.eco_temperature_high_c, 30)
+        self.assertAlmostEqual(t.eco_temperature_high_c, 30)
 
         t.set_temperature_scale("K")
         t.set_eco_temperature_high(value=290)
-        self.assertEqual(t._eco_temperature_high, 290)
+        self.assertAlmostEqual(t._eco_temperature_high, 290)
 
     def test_set_eco_temperature_low(self):
         t = Thermostat.NestThermostat()
 
         t.set_temperature_scale("F")
         t.set_eco_temperature_low(value=70)
-        self.assertEqual(t.eco_temperature_low_f, 70)
+        self.assertAlmostEqual(t.eco_temperature_low_f, 70)
 
         t.set_temperature_scale("C")
         t.set_eco_temperature_low(value=30)
-        self.assertEqual(t.eco_temperature_low_c, 30)
+        self.assertAlmostEqual(t.eco_temperature_low_c, 30)
 
         t.set_temperature_scale("K")
         t.set_eco_temperature_low(value=290)
-        self.assertEqual(t._eco_temperature_low, 290)
+        self.assertAlmostEqual(t._eco_temperature_low, 290)
 
     def test_set_hvac_mode(self):
         t = Thermostat.NestThermostat()
@@ -107,60 +107,60 @@ class TestThermostats(unittest.TestCase):
 
         t.set_temperature_scale("F")
         t.set_locked_temp_max(value=90)
-        self.assertEqual(t.locked_temp_max_f, 90)
+        self.assertAlmostEqual(t.locked_temp_max_f, 90)
 
         t.set_temperature_scale("C")
         t.set_locked_temp_max(value=30)
-        self.assertEqual(t.locked_temp_max_c, 30)
+        self.assertAlmostEqual(t.locked_temp_max_c, 30)
 
         t.set_temperature_scale("K")
         t.set_locked_temp_max(value=290)
-        self.assertEqual(t._locked_temp_max, 290)
+        self.assertAlmostEqual(t._locked_temp_max, 290)
 
     def test_set_locked_temp_min(self):
         t = Thermostat.NestThermostat()
 
         t.set_temperature_scale("F")
         t.set_locked_temp_min(value=60)
-        self.assertEqual(t.locked_temp_min_f, 60)
+        self.assertAlmostEqual(t.locked_temp_min_f, 60)
 
         t.set_temperature_scale("C")
         t.set_locked_temp_min(value=10)
-        self.assertEqual(t.locked_temp_min_c, 10)
+        self.assertAlmostEqual(t.locked_temp_min_c, 10)
 
         t.set_temperature_scale("K")
         t.set_locked_temp_min(value=260)
-        self.assertEqual(t._locked_temp_min, 260)
+        self.assertAlmostEqual(t._locked_temp_min, 260)
 
     def test_set_target_temperature_high(self):
         t = Thermostat.NestThermostat()
 
         t.set_temperature_scale("F")
         t.set_target_temperature_high(value=90)
-        self.assertEqual(t.target_temperature_high_f, 90)
+        self.assertAlmostEqual(t.target_temperature_high_f, 90)
 
         t.set_temperature_scale("C")
         t.set_target_temperature_high(value=30)
-        self.assertEqual(t.target_temperature_high_c, 30)
+        self.assertAlmostEqual(t.target_temperature_high_c, 30)
 
         t.set_temperature_scale("K")
         t.set_target_temperature_high(value=290)
-        self.assertEqual(t._target_temperature_high, 290)
+        self.assertAlmostEqual(t._target_temperature_high, 290)
 
     def test_set_target_temperature_low(self):
         t = Thermostat.NestThermostat()
 
         t.set_temperature_scale("F")
         t.set_target_temperature_low(value=60)
-        self.assertEqual(t.target_temperature_low_f, 60)
+        self.assertAlmostEqual(t.target_temperature_low_f, 60)
 
         t.set_temperature_scale("C")
         t.set_target_temperature_low(value=10)
-        self.assertEqual(t.target_temperature_low_c, 10)
+        self.assertAlmostEqual(t.target_temperature_low_c, 10)
 
         t.set_temperature_scale("K")
         t.set_target_temperature_low(value=260)
-        self.assertEqual(t._target_temperature_low, 260)
+        self.assertAlmostEqual(t._target_temperature_low, 260)
 
     def test_set_label(self):
         t = Thermostat.NestThermostat()
@@ -173,27 +173,27 @@ class TestThermostats(unittest.TestCase):
 
     def test_fahrenheit_to_celsius(self):
         value = Thermostat.fahrenheit_to_celsius(32)
-        self.assertEqual(value, 0)
+        self.assertAlmostEqual(value, 0)
         value = Thermostat.fahrenheit_to_celsius(212)
-        self.assertEqual(value, 100)
+        self.assertAlmostEqual(value, 100)
 
     def test_celsius_to_fahrenheit(self):
         value = Thermostat.celsius_to_fahrenheit(0)
-        self.assertEqual(value, 32)
+        self.assertAlmostEqual(value, 32)
         value = Thermostat.celsius_to_fahrenheit(100)
-        self.assertEqual(value, 212)
+        self.assertAlmostEqual(value, 212)
 
     def test_celsius_to_kelvin(self):
         value = Thermostat.celsius_to_kelvin(0)
-        self.assertEqual(value, 273.15)
+        self.assertAlmostEqual(value, 273.15)
         value = Thermostat.celsius_to_kelvin(100)
-        self.assertEqual(value, 373.15)
+        self.assertAlmostEqual(value, 373.15)
 
     def test_kelvin_to_celsius(self):
         value = Thermostat.kelvin_to_celsius(273.15)
-        self.assertEqual(value, 0)
+        self.assertAlmostEqual(value, 0)
         value = Thermostat.kelvin_to_celsius(373.15)
-        self.assertEqual(value, 100)
+        self.assertAlmostEqual(value, 100)
 
 
 if __name__ == "__main__":
