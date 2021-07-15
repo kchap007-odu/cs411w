f98e8b04c4d530d34db56d00ccce6bfdd24f2680
kchappel <kchap007@odu.edu>
2021-07-14 20:54:10 -0400

    Update smartdevice
    Updated smartdevice to work with new
    Thermostat class.

--- smartdevice.py
+++ smartdevice.py
@@ -2,11 +2,18 @@ from flask import Flask
 # from flask import request
 # from flask import jsonify
 
-import Devices
+import Thermostat
 
 app = Flask(__name__)
 
-sd = Devices.Thermostat()
+sd = Thermostat.NestThermostat(location="Hallway")
+sd.set_label("Nest")
+sd.set_hvac_mode("cool")
+sd.set_temperature_scale("F")
+sd._ambient_temperature = Thermostat.celsius_to_kelvin(
+    Thermostat.fahrenheit_to_celsius(65))
+sd.set_target_temperature_high(70)
+sd.set_target_temperature_low(60)
 
 
 @app.route("/device", methods=['GET', 'POST'])
