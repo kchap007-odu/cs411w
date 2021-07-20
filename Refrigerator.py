d631dc2d8f300bd6bcb1ab0612da94fc946cd0e6
Shyam Dhingani <sdhin001@odu.edu>
2021-07-20 16:36:33 -0400

    fix bug

--- Refrigerator.py
+++ Refrigerator.py
@@ -1,27 +1,19 @@
-import datetime
 from random import randrange
 
 
 class Refrigerator:
     def __init__(self, current_fridge_temperature, target_fridge_temperature, current_freezer_temperature,
-                 target_freezer_temperature, fridge_last_on_time, fridge_last_off_time, freezer_last_on_time,
-                 freezer_last_off_time, energy_use):
+                 target_freezer_temperature, energy_use):
         self.current_fridge_temperature = current_fridge_temperature
         self.target_fridge_temperature = target_fridge_temperature
         self.current_freezer_temperature = current_freezer_temperature
         self.target_freezer_temperature = target_freezer_temperature
-        self.fridge_last_on_time = fridge_last_on_time
-        self.fridge_last_off_time = fridge_last_off_time
-        self.freezer_last_on_time = freezer_last_on_time
-        self.freezer_last_off_time = freezer_last_off_time
         self.energy_use = energy_use
 
-# random times for fridge_last_on_time and fridge_last_off_time
-hours = randrange(1, 25)
-minutes = randrange(1, 61)
-seconds = randrange(1, 61)
+    time = ["Fridge_last_on_time", "Fridge_last_off_time", "Freezer_last_on_time", "Freezer_last_off_time"]
 
-print(f"First Time on {hours}:{minutes}:{seconds}")
-print(f"First Time off {hours}:{minutes}:{seconds}")
-print(f"Second Time on {hours}:{minutes}:{seconds}")
-print(f"Second Time off {hours}:{minutes}:{seconds}")
+    for currentonoff in time:
+        hours = randrange(1, 25)
+        minutes = randrange(1, 61)
+        seconds = randrange(1, 61)
+        print(currentonoff + " " + str(hours) + ":" + str(minutes) + ":" + str(seconds))
