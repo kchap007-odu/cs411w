1922153bb1682c1b48d9dea02c7334c234b6197e
Shyam Dhingani <sdhin001@odu.edu>
2021-07-20 16:53:09 -0400

    Added Project Description

--- Refrigerator.py
+++ Refrigerator.py
@@ -10,20 +10,6 @@ class Refrigerator:
         self.target_freezer_temperature = target_freezer_temperature
         self.energy_use = energy_use
 
-
-if __name__ == '__main__':
-    current_fridge_temperature = randrange(30, 35)
-    target_fridge_temperature = randrange(35, 40)
-    current_freezer_temperature = randrange(5, 10)
-    target_freezer_temperature = randrange(0, 4)
-    energy_use = randrange(100,400)
-
-    print(f"Current Fridge Temperature: {current_fridge_temperature}°F")
-    print(f"Target Fridge Temperature: {target_fridge_temperature}°F")
-    print(f"Current Freezer Temperature: {current_freezer_temperature}°F")
-    print(f"Target Freezer Temperature: {target_freezer_temperature}°F")
-    print(f"Energy Use: {energy_use} watts")
-
     time = ["Fridge_last_on_time", "Fridge_last_off_time", "Freezer_last_on_time", "Freezer_last_off_time"]
 
     for currentonoff in time:
