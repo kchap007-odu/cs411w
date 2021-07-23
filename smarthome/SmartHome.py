f6a85a4526d642e8e24205a8393965b8522dba17
kchappel <kchap007@odu.edu>
2021-07-23 17:12:25 -0400

    Add documentation and tests for SmartHome
    Added docstrings to SmartHome methods and added some unittests
    for SmartHome using hamcrest matchers.

--- smarthome/SmartHome.py
+++ smarthome/SmartHome.py
@@ -29,61 +29,101 @@ LOGGER.addHandler(ch)
 
 
 class SmartHome():
-    """
-    A class to represent a SmartHome network. Configurations can be
+    """A class to represent a SmartHome network. Configurations can be
     saved and reloaded through a config file.
 
     Parameters:
-    config_name (str): The name of the configuration file containing
-    the smart home configuration.
+        config_name (str): The name of the configuration file containing
+        the smart home configuration.
     """
-    _config_name: str = ""
 
     def __init__(self, config_name: str = ""):
+        self._config_name: str = config_name
+
         config = configparser.ConfigParser()
         config.read(config_name)
         self.devices: List[SUPPORTED_DEVICES] = []
         if config.sections():
             self._construct_devices(config)
         else:
-            print("Configuration file is empty")
+            LOGGER.warning("Configuration file is empty")
 
     def _construct_devices(self, config: configparser.ConfigParser):
         """Constructs devices from a saved configuration in .ini file.
         This function assumes all classes have setters for corresponding
         values following a "set_<name>" format.
 
-        Parameters:
-        config (configparser.ConfigParser): The configuration file
-        containing smart device information associated with the home.
+        Args:
+            config (configparser.ConfigParser): The configuration file
+            containing smart device information associated with the
+            home.
         """
         for section in config.sections():
             items = dict(config.items(section))
-            # TODO: Enforce requirement for class to be defined in config.
+            # TODO: Enforce requirement for class to be in config.
             class_ = items["class"]
             # Remove class since it is not a property of the device.
             del items["class"]
             # Create the device from the stored class name.
             device = create_class_from_name(class_)
-            print(type(device))
             for item in items.keys():
                 command = f"device.set_{item}({items[item]})"
                 # print(command)
                 eval(command)
-            self.devices.append(device)
+            self.add_new_device(device)
 
     def add_new_device(self, device: SUPPORTED_DEVICES = None):
+        """Adds a device to the list of devices in the home.
+
+        Args:
+            device (SUPPORTED_DEVICES, optional): The device to add.
+            Defaults to None.
+        """
         self.devices.append(device)
 
-    def get_device_from_device_id(self, device_id: str = ""):
-        for device in self.devices:
-            if device.device_id == device:
-                return device
+    @property
+    def num_devices(self) -> int:
+        """Returns the number of devices associated with the smart home.
 
-    def get_devices_by_type(self, type_: str = ""):
+        Returns:
+            int: The number of devices associated with the home.
+        """
+        return len(self.devices)
+
+    def get_device_by_device_id(self, device_id: str = "") -> SUPPORTED_DEVICES:
+        """Gets a device by device ID.
+
+        Args:
+            device_id (str, optional): The unique identifier for the
+            device. Defaults to "".
+
+        Returns:
+            Union[Devices.SmartDevice, Light.PhilipsHueLamp,
+            Refrigerator.Refrigerator, Thermostat.NestThermostat,
+            water_heater.water_heater, None]: The device matching the
+            device ID.
+        """
+        for device in self.devices:
+            if device.device_id == device_id:
+                return device
+        return None
+
+    def get_devices_by_type(self, type_: str = "") -> List:
+        """Gets all devices in the smart home matching the device type.
+
+        Args:
+            type_ (str, optional): The type of devices to get, e.g.,
+            "Light". Defaults to "".
+
+        Returns:
+            List: A list of all devices with a matching type
+        """
         return [i for i in self.devices if i.device_type == type]
 
     def write_config_file(self):
+        """Stores the current smart home's device list and states as a
+        configuration file.
+        """
         for i, device in enumerate(self.devices, start=1):
             print(f"[device{i}]")
             params = device.get_settable_parameters()
@@ -98,7 +138,7 @@ def create_class_from_name(name: str = "") -> SUPPORTED_DEVICES:
     stored in the ini file.
 
     Parameters:
-    name (str): The name of the class.
+        name (str): The name of the class.
     """
     if name == "NestThermostat":
         return Thermostat.NestThermostat()
---
+++ tests/TestSmartHome.py
@@ -0,0 +1,47 @@
+import os
+import unittest
+
+from hamcrest import assert_that, equal_to, is_, instance_of
+
+from smarthome.SmartHome import SmartHome
+from devices.Thermostat import NestThermostat
+
+
+class TestSmartHome(unittest.TestCase):
+    def test_constructor(self):
+        sh = SmartHome()
+        assert_that(sh.num_devices, is_(equal_to(0)))
+
+        config_file = os.path.join(
+            os.path.dirname(__file__), 'two-thermostats.ini')
+        sh = SmartHome(config_name=config_file)
+        assert_that(sh.num_devices, is_(equal_to(2)))
+
+    def test_add_device(self):
+        t = NestThermostat()
+
+        device_id = t.device_id
+        sh = SmartHome()
+
+        sh.add_new_device(t)
+        device = sh.get_device_by_device_id(device_id)
+
+        assert_that(device, is_(instance_of(NestThermostat)))
+
+        assert_that(sh.num_devices, is_(equal_to(1)))
+        assert_that(t, is_(equal_to(device)))
+
+        # TODO: Add more tests.
+
+    def test_write_config(self):
+        sh = SmartHome()
+        t1 = NestThermostat(name="Nest", location="Hallway")
+        t2 = NestThermostat(name="Nest", location="Living Room")
+        sh.add_new_device(t1)
+        sh.add_new_device(t2)
+
+        assert_that(sh.num_devices, is_(equal_to(2)))
+
+
+if __name__ == "__main__":
+    unittest.main()
