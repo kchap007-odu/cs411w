f16359246e8c02284f54a3a5cda9176bfa5d230c
Shyam Dhingani <sdhin001@odu.edu>
2021-07-25 14:26:19 -0400

    add rest of the passed tests

--- tests/TestRefrigerator.py
+++ tests/TestRefrigerator.py
@@ -4,12 +4,20 @@ from devices.Refrigerator import Refrigerator
 
 class TestRefrigerator(unittest.TestCase):
 
-
     def setUp(self):
         self.rf = Refrigerator()
 
     def test_current_fridge_temperature(self):
         self.assertTrue(self.rf.current_fridge_temperature() >= 30 and self.rf.current_fridge_temperature() <= 35)
+
+    def test_target_fridge_temperature(self):
+        self.assertTrue(self.rf.target_fridge_temperature() >= 35 and self.rf.target_fridge_temperature() <= 40)
+
+    def test_current_freezer_temperature(self):
+        self.assertTrue(self.rf.current_freezer_temperature() >= 5 and self.rf.current_freezer_temperature() <= 10)
+
+    def test_target_freezer_temperature(self):
+        self.assertTrue(self.rf.target_freezer_temperature() >= 0 and self.rf.target_freezer_temperature() <= 4)
 
     def test_energy_use(self):
         self.assertTrue(self.rf.energy_use() >= 100 and self.rf.energy_use() <= 400)
