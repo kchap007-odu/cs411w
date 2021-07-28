ec2ee8ab9965c07b0847a45b74ffeeb83f4fcb24
kchappel <kchap007@odu.edu>
2021-07-27 22:33:48 -0400

    Significant refactor of SmartHome
    Significantly refactored SmartHome to be more Pythonic and make
    structure more similar to smart devices.

--- smarthome/SmartHome.py
+++ smarthome/SmartHome.py
@@ -1,98 +1,154 @@
 import json
-import logging
+import time
 
 from typing import List, Union
 
-from devices import Devices, Light, Refrigerator, Thermostat, water_heater
+from devices.Light import PhilipsHueLamp
+from devices.Thermostat import NestThermostat
+from devices.Faucet import Faucet
+from devices.Refrigerator import Refrigerator
+from devices.water_heater import water_heater
 
-SUPPORTED_DEVICES = Union[
-    Devices.SmartDevice,
-    Light.PhilipsHueLamp,
-    Refrigerator.Refrigerator,
-    Thermostat.NestThermostat,
-    water_heater.water_heater
+from helpers.misc import json_from_file
+
+SupportedDevices = Union[
+    PhilipsHueLamp,
+    Refrigerator,
+    NestThermostat,
+    Faucet,
+    water_heater
 ]
 
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    '%(asctime)s - %(filename)s - %(lineno)s - %(levelname)s - %(message)s')
-fh = logging.FileHandler('SmartDevice.log')
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-
-ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
-ch.setFormatter(formatter)
-LOGGER.addHandler(fh)
-LOGGER.addHandler(ch)
+SupportedDevicesString = [
+    "PhilipsHueLamp",
+    "Refrigerator",
+    "NestThermostat",
+    "Faucet",
+    "water_heater"
+]
 
 
 class SmartHome():
     """A class to represent a SmartHome network. Configurations can be
     saved and reloaded through a config file.
 
+    Properties based on Honewell Home response snippets found here:
+    https://developer.honeywellhome.com/content/t-series-thermostat-guide
+
     Parameters:
-        config_name (str): The name of the configuration file containing
-        the smart home configuration.
+        json_file (str): The fully qualified path to the file containing
+        the configuration data.
     """
 
     def __init__(self, json_file: str = None):
-        self._config_name: str = json_file
+        # TODO: Need to convert from pascalCase to snake_case on input.
+        self.set_location_id(hash(time.time()))
+        self.set_name("None")
+        self.set_street_address("None")
+        self.set_city("None")
+        self.set_state("None")
+        self.set_country("None")
+        self.set_zipcode("None")
+        self.set_config_name: Union(str, None) = json_file
 
-        self.devices: List[SUPPORTED_DEVICES] = []
+        self._devices: List[SupportedDevices] = []
+
         if json_file is not None:
-            self._construct_devices_from_json(json_file)
+            json_data = json_from_file(json_file)
+            self.__from_json__(json_data)
 
-    def _construct_devices_from_json(self, filename: str = ""):
-        with open(filename) as f:
-            d = json.loads(f.read())
-        # FIXME: Rewrite this using dictionary.pop() method.
-        if "devices" in d.keys():
-            devices = d["devices"]
-        for device in devices.keys():
-            device_class = devices[device]["class"]
-            d = create_class_from_name(device_class)
-            d.__from_json__(devices[device])
-            self.add_new_device(d)
+    def __from_json__(self, json_data: dict):
+        # Set up the device properties.
+        available_devices = json_data.pop("devices", None)
+        if available_devices is not None:
+            for v in available_devices.values():
+                self.append(device_factory(v.pop("class"), v))
 
-    def add_new_device(self, device: SUPPORTED_DEVICES = None):
-        """Adds a device to the list of devices in the home.
+        # Set the home properties.
+        properties = dir(self)
+        for k in json_data.keys():
+            parameter = f"set_{k}"
+            if parameter in properties:
+                eval(f"self.{parameter}(json_data['{k}'])")
 
-        Args:
-            device (SUPPORTED_DEVICES, optional): The device to add.
-            Defaults to None.
-        """
-        self.devices.append(device)
-
-    @property
-    def num_devices(self) -> int:
-        """Returns the number of devices associated with the smart home.
-
-        Returns:
-            int: The number of devices associated with the home.
-        """
-        return len(self.devices)
-
-    def get_device_by_device_id(self, device_id: str = "") -> SUPPORTED_DEVICES:
-        """Gets a device by device ID.
-
-        Args:
-            device_id (str, optional): The unique identifier for the
-            device. Defaults to "".
-
-        Returns:
-            Union[Devices.SmartDevice, Light.PhilipsHueLamp,
-            Refrigerator.Refrigerator, Thermostat.NestThermostat,
-            water_heater.water_heater, None]: The device matching the
-            device ID.
-        """
-        for device in self.devices:
-            if device.device_id == device_id:
-                return device
+    def __getitem__(self, key: Union[int, str]) -> SupportedDevices:
+        if isinstance(key, str):
+            # Do search by device id
+            for device in self._devices:
+                if device.device_id == key:
+                    return device
+        elif isinstance(key, int):
+            return self._devices[key]
         return None
 
-    def get_devices_by_type(self, type_: str = "") -> List:
+    def __len__(self):
+        return len(self._devices)
+
+    def __setitem__(self, key: Union[int, None] = None,
+                    value: SupportedDevices = None):
+        if key is not None:
+            self._devices[key] = value
+
+    def __str__(self):
+        dictionary = {}
+        for i, device in enumerate(self._devices, start=1):
+            dictionary[f"device{i}"] = device.__properties__()
+
+        return json.dumps(dictionary, indent=4)
+
+    def append(self, item: SupportedDevices):
+        self._devices.append(item)
+
+    @ property
+    def city(self) -> str:
+        return self._city
+
+    def set_city(self, city: str):
+        self._city = city
+
+    @ property
+    def country(self) -> str:
+        return self._country
+
+    def set_country(self, country: str):
+        self._country = country
+
+    @ property
+    def location_id(self) -> str:
+        return self._location_id
+
+    def set_location_id(self, location_id: int):
+        self._location_id = location_id
+
+    @ property
+    def name(self) -> str:
+        return self._name
+
+    def set_name(self, name: str):
+        self._name = name
+
+    @ property
+    def state(self) -> str:
+        return self._state
+
+    def set_state(self, state: str):
+        self._state = state
+
+    @ property
+    def street_address(self) -> str:
+        return self._street_address
+
+    def set_street_address(self, street_address: str):
+        self._street_address = street_address
+
+    @ property
+    def zipcode(self) -> str:
+        return self._zipcode
+
+    def set_zipcode(self, zipcode: str):
+        self._zipcode = zipcode
+
+    def get_devices_by_type(self, type_: str = "") -> List[SupportedDevices]:
         """Gets all devices in the smart home matching the device type.
 
         Args:
@@ -100,36 +156,26 @@ class SmartHome():
             "Light". Defaults to "".
 
         Returns:
-            List: A list of all devices with a matching type
+            List: A list of all devices with a matching type.
         """
-        return [i for i in self.devices if i.device_type == type_]
-
-    def write_config_file(self):
-        """Stores the current smart home's device list and states as a
-        configuration file.
-        """
-        dictionary = {}
-        for i, device in enumerate(self.devices, start=1):
-            dictionary[f"device{i}"] = device.__properties__()
-
-        # print(json.dumps(dictionary, indent=4))
+        return [i for i in self._devices if i.device_type == type_]
 
 
-def create_class_from_name(name: str = "") -> SUPPORTED_DEVICES:
+def device_factory(name: str = "", config: dict = None) -> SupportedDevices:
     """Helper function to construct supported classes from class names
     stored in the ini file.
 
-    Parameters:
-        name (str): The name of the class.
+    Args:
+        name (str, optional): The name of the class to create. Defaults
+        to "".
+
+    Returns:
+        SupportedDevices: The class of device specified by name.
     """
-    if name == "NestThermostat":
-        return Thermostat.NestThermostat()
-    elif name == "PhillipsHueLamp":
-        return Light.PhilipsHueLamp()
-    elif name == "SmartDevice":
-        return Devices.SmartDevice()
+    if name in SupportedDevicesString:
+        device = eval(f"{name}()")
 
+    if config is not None:
+        device.__from_json__(config)
 
-if __name__ == "__main__":
-    sh = SmartHome(config_name="test-config.ini")
-    sh.write_config_file()
+    return device
--- tests/test_smarthome.py
+++ tests/test_smarthome.py
@@ -5,7 +5,8 @@ from hamcrest import assert_that, equal_to, is_, instance_of
 
 from smarthome.SmartHome import SmartHome
 from devices.Thermostat import NestThermostat
-from devices.Light import PhilipsHueLamp
+
+from helpers.misc import json_from_file
 
 
 class TestSmartHome(unittest.TestCase):
@@ -25,7 +26,7 @@ class TestSmartHome(unittest.TestCase):
         sh = SmartHome()
 
         sh.append(t)
-        device = sh.get_device_by_device_id(device_id)
+        device = sh[device_id]
 
         assert_that(device, is_(instance_of(NestThermostat)))
 
@@ -41,31 +42,13 @@ class TestSmartHome(unittest.TestCase):
         devices = sh.get_devices_by_type("Thermostat")
         assert_that(len(devices), is_(equal_to(2)))
 
-    def test_get_device_by_id(self):
-        config_file = os.path.join(
-            os.path.dirname(__file__), 'test-smart-home.json')
-        sh = SmartHome(json_file=config_file)
-
-        device = sh["1234"]
-        assert_that(device, is_(instance_of(NestThermostat)))
-        assert_that(device.device_id, is_(equal_to("1234")))
-
-        device2 = sh["2345"]
-        assert_that(device2, is_(instance_of(NestThermostat)))
-        assert_that(device2.device_id, is_(equal_to("2345")))
-
-        device3 = sh["3456"]
-        assert_that(device3, is_(instance_of(PhilipsHueLamp)))
-        assert_that(device3.device_id, is_(equal_to("3456")))
-
-        device_does_not_exist = sh["4567"]
-        assert_that(device_does_not_exist, is_(None))
-
     def test_construct_devices_from_json(self):
         config_file = os.path.join(
             os.path.dirname(__file__), 'test-smart-home.json')
+        json_data = json_from_file(config_file)
+
         sh = SmartHome()
-        sh._construct_devices_from_json(filename=config_file)
+        sh.__from_json__(json_data)
         assert_that(len(sh), is_(equal_to(3)))
 
     def test_write_config(self):
