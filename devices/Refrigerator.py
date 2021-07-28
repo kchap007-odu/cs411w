d38b6a7a2491794423164df35110b8a43868d7f2
Shyam Dhingani <sdhin001@odu.edu>
2021-07-28 15:35:38 -0400

    Change the number of output data

--- devices/Refrigerator.py
+++ devices/Refrigerator.py
@@ -19,22 +19,25 @@ class Refrigerator:
 
 
 if __name__ == '__main__':
-    current_fridge_temperature = randrange(30, 35)
-    target_fridge_temperature = randrange(35, 40)
-    current_freezer_temperature = randrange(5, 10)
-    target_freezer_temperature = randrange(0, 4)
-    energy_use = randrange(100, 400)
 
-    print(f"Current Fridge Temperature: {current_fridge_temperature}°F")
-    print(f"Target Fridge Temperature: {target_fridge_temperature}°F")
-    print(f"Current Freezer Temperature: {current_freezer_temperature}°F")
-    print(f"Target Freezer Temperature: {target_freezer_temperature}°F")
-    print(f"Energy Use: {energy_use} watts")
+    num = 5
+    for x in range(num):
+        current_fridge_temperature = randrange(30, 35)
+        target_fridge_temperature = randrange(35, 40)
+        current_freezer_temperature = randrange(5, 10)
+        target_freezer_temperature = randrange(0, 4)
+        energy_use = randrange(100, 400)
 
-    time = ["Fridge_last_on_time", "Fridge_last_off_time", "Freezer_last_on_time", "Freezer_last_off_time"]
+        print(f"Current Fridge Temperature: {current_fridge_temperature}°F")
+        print(f"Target Fridge Temperature: {target_fridge_temperature}°F")
+        print(f"Current Freezer Temperature: {current_freezer_temperature}°F")
+        print(f"Target Freezer Temperature: {target_freezer_temperature}°F")
+        print(f"Energy Use: {energy_use} watts")
 
-    for currentonoff in time:
-        hours = randrange(1, 25)
-        minutes = randrange(1, 61)
-        seconds = randrange(1, 61)
-        print(currentonoff + " " + str(hours) + ":" + str(minutes) + ":" + str(seconds))
+        time = ["Fridge_last_on_time", "Fridge_last_off_time", "Freezer_last_on_time", "Freezer_last_off_time"]
+
+        for currentonoff in time:
+            hours = randrange(1, 25)
+            minutes = randrange(1, 61)
+            seconds = randrange(1, 61)
+            print(currentonoff + " " + str(hours) + ":" + str(minutes) + ":" + str(seconds))
