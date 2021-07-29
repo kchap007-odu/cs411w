845a9597d306bce662b015da3074483490c83fe9
Shyam Dhingani <sdhin001@odu.edu>
2021-07-28 20:13:48 -0400

    adding json module

--- devices/Refrigerator.py
+++ devices/Refrigerator.py
@@ -1,5 +1,5 @@
 from random import randrange
-
+import json
 
 class Refrigerator:
     def current_fridge_temperature(self):
@@ -19,25 +19,15 @@ class Refrigerator:
 
 
 if __name__ == '__main__':
-
     num = 5
+    # defining an empty list
+    results = []
     for x in range(num):
+        # creating an empty dict
+        data = {}
+        # defining all the attributes needed, using random numbers
         current_fridge_temperature = randrange(30, 35)
         target_fridge_temperature = randrange(35, 40)
         current_freezer_temperature = randrange(5, 10)
         target_freezer_temperature = randrange(0, 4)
         energy_use = randrange(100, 400)
-
-        print(f"Current Fridge Temperature: {current_fridge_temperature}°F")
-        print(f"Target Fridge Temperature: {target_fridge_temperature}°F")
-        print(f"Current Freezer Temperature: {current_freezer_temperature}°F")
-        print(f"Target Freezer Temperature: {target_freezer_temperature}°F")
-        print(f"Energy Use: {energy_use} watts")
-
-        time = ["Fridge_last_on_time", "Fridge_last_off_time", "Freezer_last_on_time", "Freezer_last_off_time"]
-
-        for currentonoff in time:
-            hours = randrange(1, 25)
-            minutes = randrange(1, 61)
-            seconds = randrange(1, 61)
-            print(currentonoff + " " + str(hours) + ":" + str(minutes) + ":" + str(seconds))
