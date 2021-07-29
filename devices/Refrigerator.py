5dec8abaf692a528e2cd75391a8b47384f416475
Shyam Dhingani <sdhin001@odu.edu>
2021-07-28 20:16:03 -0400

    json inputs added

--- devices/Refrigerator.py
+++ devices/Refrigerator.py
@@ -35,3 +35,13 @@ if __name__ == '__main__':
         fridge_last_off_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
         freezer_last_on_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
         freezer_last_off_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
+
+        data["Current Fridge Temperature"] = current_fridge_temperature
+        data["Target Fridge Temperature"] = target_fridge_temperature
+        data["Current Freezer Temperature"] = current_freezer_temperature
+        data["Target Freezer Temperature"] = target_freezer_temperature
+        data["Energy Use"] = energy_use
+        data["Fridge Last On Time"] = fridge_last_on_time
+        data["Fridge Last Off Time"] = fridge_last_off_time
+        data["Freezer Last On Time"] = freezer_last_on_time
+        data["Freezer Last Off Time"] = freezer_last_off_time
