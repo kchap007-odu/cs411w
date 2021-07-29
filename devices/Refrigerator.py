c4f7958a725b75cd0f13adf5a3612983c4084c50
Shyam Dhingani <sdhin001@odu.edu>
2021-07-28 20:22:14 -0400

    adding comments

--- devices/Refrigerator.py
+++ devices/Refrigerator.py
@@ -1,6 +1,7 @@
 from random import randrange
 import json
 
+
 class Refrigerator:
     def current_fridge_temperature(self):
         return randrange
@@ -20,12 +21,12 @@ class Refrigerator:
 
 if __name__ == '__main__':
     num = 5
-
+    # defining an empty list
     results = []
     for x in range(num):
-
+        # creating an empty dict
         data = {}
-
+        # defining all the attributes needed, using random numbers
         current_fridge_temperature = randrange(30, 35)
         target_fridge_temperature = randrange(35, 40)
         current_freezer_temperature = randrange(5, 10)
@@ -35,7 +36,7 @@ if __name__ == '__main__':
         fridge_last_off_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
         freezer_last_on_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
         freezer_last_off_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
-
+        # adding each attribute to data dict, with proper key (name of attribute)
         data["Current Fridge Temperature"] = current_fridge_temperature
         data["Target Fridge Temperature"] = target_fridge_temperature
         data["Current Freezer Temperature"] = current_freezer_temperature
@@ -45,3 +46,7 @@ if __name__ == '__main__':
         data["Fridge Last Off Time"] = fridge_last_off_time
         data["Freezer Last On Time"] = freezer_last_on_time
         data["Freezer Last Off Time"] = freezer_last_off_time
+        # adding data dict to results list
+        results.append(data)
+    # printing results list of dicts in json format, with 4 as indentation
+    print(json.dumps(results, indent=4))
