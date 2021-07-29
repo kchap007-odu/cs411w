969a4d8b4e396e90875dac0124f13afb890144d1
Shyam Dhingani <sdhin001@odu.edu>
2021-07-28 20:15:13 -0400

    fridge/freezer times

--- devices/Refrigerator.py
+++ devices/Refrigerator.py
@@ -20,14 +20,18 @@ class Refrigerator:
 
 if __name__ == '__main__':
     num = 5
-    # defining an empty list
+
     results = []
     for x in range(num):
-        # creating an empty dict
+
         data = {}
-        # defining all the attributes needed, using random numbers
+
         current_fridge_temperature = randrange(30, 35)
         target_fridge_temperature = randrange(35, 40)
         current_freezer_temperature = randrange(5, 10)
         target_freezer_temperature = randrange(0, 4)
         energy_use = randrange(100, 400)
+        fridge_last_on_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
+        fridge_last_off_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
+        freezer_last_on_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
+        freezer_last_off_time = str(randrange(1, 25)) + ":" + str(randrange(1, 61)) + ":" + str(randrange(1, 61))
