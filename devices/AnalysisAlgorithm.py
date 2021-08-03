a7ab75334db762910a54ff0d583ae05c4eb77e44
Shyam Dhingani <sdhin001@odu.edu>
2021-08-03 18:01:59 -0400

    Database connection and changes to algorithm

--- devices/AnalysisAlgorithm.py
+++ devices/AnalysisAlgorithm.py
@@ -3,6 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# function definition that takes Temp_xAxis and Mean_yAxis values as input and plot graph using pyplot
+
+def plot_graph(Temp_xAxis, Mean_yAxis):
+    plt.bar(Temp_xAxis, Mean_yAxis)
+    plt.title('Mean')
+    plt.xticks(rotation=45)
+    plt.show()
+
+
 read = pd.read_json('data_chart.json')
 
 current_fridge_temperature_mean = np.mean(read['Current Fridge Temperature'])
@@ -11,12 +20,11 @@ current_freezer_temperature_mean = np.mean(read['Current Freezer Temperature'])
 target_freezer_temperature_mean = np.mean(read['Target Freezer Temperature'])
 energy_use_mean = np.mean(read['Energy Use'])
 
-xAxis = ['Current Fridge Temperature', 'Target Fridge Temperature',
+Temp_xAxis = ['Current Fridge Temperature', 'Target Fridge Temperature',
          'Current Freezer Temperature', 'Target Freezer Temperature']
-yAxis = [current_fridge_temperature_mean, target_fridge_temperature_mean,
+Mean_yAxis = [current_fridge_temperature_mean, target_fridge_temperature_mean,
          current_freezer_temperature_mean, target_freezer_temperature_mean]
 
-plt.bar(xAxis, yAxis)
-plt.title('Mean')
-plt.xticks(rotation=45)
-plt.show()
+# calling function to plot the graph with parameters Temp_xAxis and Mean_yAxis
+
+plot_graph(Temp_xAxis, Mean_yAxis)
