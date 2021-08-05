ad309fe35f08a672a611747a4f4228666ef95f48
Shyam Dhingani <sdhin001@odu.edu>
2021-08-05 19:06:25 -0400

    Added comment to ConnectionTest.py

--- ConnectionTest.py
+++ ConnectionTest.py
@@ -5,7 +5,6 @@ connection = pymysql.connect(host='sql5.freesqldatabase.com', user='sql5428936',
                              password='bARWMcterL', database='sql5428936')
 
 cursor = connection.cursor()
-
 sql_query = "SELECT VERSION()"
 
 try:
@@ -17,3 +16,5 @@ except Exception as e:
     print("Exception :", e)
 
 connection.close()
+
+# works and prints out Database version : 5.5.62-0ubuntu0.14.04.1
