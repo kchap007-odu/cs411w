d57766c8b67530d84be756a990f282f760ae4ed3
Shyam Dhingani <sdhin001@odu.edu>
2021-08-05 15:59:08 -0400

    add connection details for database

--- ConnectionTest.py
+++ ConnectionTest.py
@@ -1,13 +1,41 @@
 import pyodbc
+import json
 
 
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=sql5.freesqldatabase.com;'
-                      'Database=sql5428936;'
-                      'Trusted_Connection=yes;')
-
+# connect to database
+conn = pyodbc.connect('DRIVER={SQL Server}; SERVER=sql5.freesqldatabase.com@3306; DATABASE=sql5428936; UID=sql5428936; PWD=bARWMcterL')
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM database_name.table')
 
-for row in cursor:
-    print(row)
+# create a record as python dictionary
+
+record = {
+
+"Current Fridge Temperature": 34,
+
+"Target Fridge Temperature": 38,
+
+"Current Freezer Temperature": 5,
+
+"Target Freezer Temperature": 0,
+
+"Energy Use": 397,
+
+"Fridge Last On Time": "22:46:34",
+
+"Fridge Last Off Time": "4:45:2",
+
+"Freezer Last On Time": "11:39:27",
+
+"Freezer Last Off Time": "14:39:37"
+
+}
+
+# insert this into database as json object
+
+cursor.execute("Insert Into refridgerator values (?)", (json.dumps(record),))
+cursor.commit()
+
+# close all connections
+
+cursor.close()
+conn.close()
