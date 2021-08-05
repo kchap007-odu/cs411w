cf4222569a1aed4ed52f8c119a76719097415e64
estob001 <78703455+estob001@users.noreply.github.com>
2021-08-04 21:20:01 -0400

    Update Faucet.py

--- Faucet.py
+++ Faucet.py
@@ -1,11 +1,38 @@
 import locale
 import time
+import random
 #import Devices
 
-# turn water on/off
+# turn water on/off 
+
 
 class Faucet:  
 
-    _poweron: = "on"
-    _poweroff: = "off"
-    
+    #variables
+    Power = ["on", "off"]
+
+    _pressureL: = 0
+    _pressureM: = 1
+    _pressureH: = 2
+
+    #temperature control 32F - 210F
+    _tempCurrent: 0
+    _waterusage: 0 
+
+    #Start Functions here:
+    def temperature(self):
+        return temp(32,210)
+
+
+
+    #Pressure function 
+    def pressure(self):
+        return random(0,2)
+
+
+  #water usage 
+    def waterusage(self):
+        return usage(0,100)
+
+   # def 
+
