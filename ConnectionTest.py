e18db498e45ca4c87c66e3e6f5b7f3e14956ee9a
Shyam Dhingani <sdhin001@odu.edu>
2021-08-06 16:45:52 -0400

    edit requirements.txt

--- ConnectionTest.py
+++ ConnectionTest.py
@@ -11,21 +11,22 @@ connection = pymysql.connect(host='sql5.freesqldatabase.com', user='sql5428936',
 cursor = connection.cursor()
 
 for item in json_obj:
-    Current_Fridge_Temp = item.get("Current Fridge Temperature")
-    Target_Fridge_Temp = item.get("Target Fridge Temperature")
-    Current_Freezer_Temp = item.get("Current Freezer Temperature")
-    Target_Freezer_Temp = item.get("Target Freezer Temperature")
-    E_Use = item.get("Energy Use")
+    Current_Fridge_Temp = int(item.get("Current Fridge Temperature"))
+    Target_Fridge_Temp = int(item.get("Target Fridge Temperature"))
+    Current_Freezer_Temp = int(item.get("Current Freezer Temperature"))
+    Target_Freezer_Temp = int(item.get("Target Freezer Temperature"))
+    E_Use = int(item.get("Energy Use"))
     Fridge_Last_On = item.get("Fridge Last On Time")
     Fridge_Last_Off = item.get("Fridge Last Off Time")
     Freezer_Last_On = item.get("Freezer Last On Time")
     Freezer_Last_Off = item.get("Freezer Last Off Time")
 
-    cursor.execute("insert into refrigerator_id(Current Fridge Temperature, Target Fridge Temperature, "
-                   "Current Freezer Temperature, Target Freezer Temperature, Energy Use, Fridge Last On Time, "
-                   "Fridge Last Off Time, Freezer Last On Time, Freezer Last Off Time) "
-                   "value(%s,%s,%s,%s,%s,%s,%s,%s,%s)", (Current_Fridge_Temp, Target_Fridge_Temp, Current_Freezer_Temp,
-                                                         Target_Freezer_Temp, E_Use, Fridge_Last_On, Fridge_Last_Off,
-                                                         Freezer_Last_On, Freezer_Last_Off))
+    cursor.execute("INSERT INTO refrigerator_id (Current_Fridge_Temperature, Target_Fridge_Temperature, "
+                   "Current_Freezer_Temperature, Target_Freezer_Temperature, Energy_Use, Fridge_Last_On_Time, "
+                   "Fridge_Last_Off_Time, Freezer_Last_On_Time, Freezer_Last_Off_Time) "
+                   "VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)", (Current_Fridge_Temp, Target_Fridge_Temp, Current_Freezer_Temp,
+                                                          Target_Freezer_Temp, E_Use, Fridge_Last_On, Fridge_Last_Off,
+                                                          Freezer_Last_On, Freezer_Last_Off))
+
 connection.commit()
 connection.close()
